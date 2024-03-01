79e4f23eddf6d19d2d63db1e7320b0dc2b15e790
KrokoNinja <lenni.pfundtner@gmail.com>
2024-03-01 13:19:42 +0100

    add: styles for intro heading and subtitle

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,8 @@
 export default function Hero() {
     return (
         <div className="font-overpass py-16">
-            <h1 className="text-4xl text-center font-[600] text-neutral-white mb-2">A modern <br className="lg:hidden"/> publishing platform</h1>
-            <p className="text-center text-neutral-white mb-8">Grow your audience and build your <br className="xl:hidden" /> online brand</p>
+            <h1 className="text-4xl text-center font-[600] text-neutral-white mb-2 lg:mb-3 lg:text-5xl">A modern <br className="lg:hidden"/> publishing platform</h1>
+            <p className="text-center text-neutral-white mb-8 lg:text-lg">Grow your audience and build your <br className="xl:hidden" /> online brand</p>
             <div className="flex flex-row items-center justify-center gap-x-4">
                 <button className="font-ubuntu bg-neutral-white text-primary-light-red rounded-full py-2 px-4 font-bold">Start for Free</button>
                 <button className="font-ubuntu text-neutral-white font-bold border border-neutral-white rounded-full py-2 px-5">Learn More</button>
