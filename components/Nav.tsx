ddd49a812edb51c2de02dd78d7249d01497f0c69
KrokoNinja <lenni.pfundtner@gmail.com>
2024-04-30 16:08:58 +0200

    add dropdowns in nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,47 +1,42 @@
 'use client';
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "./ui/navigation-menu";
+import { useState } from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 export default function Nav() {
+
+    const content = [
+        {
+            title: "Product",
+            links: ["Overview", "Pricing", "Marketplace", "Features", "Integrations"]
+        },
+        {
+            title: "Company",
+            links: ["About", "Team", "Blog", "Careers"]
+        },
+        {
+            title: "Connect",
+            links: ["Contact", "Newsletter", "LinkedIn"]
+        }
+    ];
+
     return (
-        //TODO: Change Content positioning to be aligned under the trigger
-        <NavigationMenu>
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                <NavigationMenuTrigger>Product</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <NavigationMenuLink href="#" className="hover:font-bold">Overview</NavigationMenuLink>
-                        <NavigationMenuLink href="#" className="hover:font-bold">Pricing</NavigationMenuLink>
-                        <NavigationMenuLink href="#" className="hover:font-bold">Marketplace</NavigationMenuLink>
-                        <NavigationMenuLink href="#" className="hover:font-bold">Features</NavigationMenuLink>
-                        <NavigationMenuLink href="#" className="hover:font-bold">Integrations</NavigationMenuLink>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                <NavigationMenuTrigger>Company</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <NavigationMenuLink href="#" className="hover:font-bold">About</NavigationMenuLink>
-                        <NavigationMenuLink href="#" className="hover:font-bold">Team</NavigationMenuLink>
-                        <NavigationMenuLink href="#" className="hover:font-bold">Blog</NavigationMenuLink>
-                        <NavigationMenuLink href="#" className="hover:font-bold">Careers</NavigationMenuLink>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                <NavigationMenuTrigger>Connect</NavigationMenuTrigger>
-                    <NavigationMenuContent>
-                        <NavigationMenuLink href="#" className="hover:font-bold">Contact</NavigationMenuLink>
-                        <NavigationMenuLink href="#" className="hover:font-bold">Newsletter</NavigationMenuLink>
-                        <NavigationMenuLink href="#" className="hover:font-bold">LinkedIn</NavigationMenuLink>
-                    </NavigationMenuContent>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-            <NavigationMenuList className="gap-x-3">
-                <NavigationMenuItem>
-                    <NavigationMenuLink href="#" className="btn button-secondary border-none button-primary-hover">Login</NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink href="#" className="btn button-primary button-primary-hover px-8 py-3">Sign Up</NavigationMenuLink>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
+        <nav className="md:flex gap-8 relative">
+            {content.map((section, index) => {
+                return (
+                    <div key={index} className="group">
+                        <p className="text-white text-lg font-semibold flex items-center gap-2 mb-2">{section.title} <FaChevronDown className="group-hover:hidden" size={12} /> <FaChevronUp className="group-hover:block hidden" size={12}/></p>
+                        <ul className="group-hover:flex flex-col gap-2 bg-white rounded-md py-3 px-5 hidden absolute">
+                            {section.links.map((link, index) => {
+                                return (
+                                    <li key={index} className="w-20">
+                                        <a href="#" className="text-black text-sm hover:font-medium">{link}</a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                )
+            })}
+        </nav>
     )
 }
